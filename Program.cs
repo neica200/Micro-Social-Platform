b6c15a550bb7c7a63d7ef5e4b1f9a142b82fa059
neica200 <marioneica200@gmail.com>
2025-11-28 01:12:11 +0200

    Login si Register pagini

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+
 //Connection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
